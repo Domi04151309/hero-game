749eb8a52ddab2ccce7a1c797825a80af65c1b6a
Domi04151309 <domi04151309business@gmail.com>
2020-03-28 15:38:49 +0100

    Add end screen

--- sketch.js
+++ sketch.js
@@ -10,6 +10,11 @@ class View {
     this.lbl8 = document.getElementById("selectedMonster");
     this.lbl9 = document.getElementById("selectedHero");
     this.btn4 = document.getElementById("run");
+
+    this.winner = document.getElementById("winner");
+    this.winnerIcon = document.getElementById("winnerIcon");
+    this.winningTeam = document.getElementById("winningTeam");
+    this.reload = document.getElementById("reload");
   }
 }
 
@@ -20,6 +25,7 @@ class Controller {
     this.view = viewToUse;
     this.model = modelToUse;
     this.view.btn4.addEventListener("click", this.btn1Pressed.bind(this));
+    this.view.reload.addEventListener("click", this.reloadPressed.bind(this));
     this.updateView();
   }
 
@@ -28,15 +34,19 @@ class Controller {
     this.view.scrollbox2.innerHTML = this.model.getMonstersCondition();
     this.view.lbl3.innerHTML = this.model.getHerosLifepoints();
     this.view.lbl5.innerHTML = this.model.getMonstersLifepoints();
-    
+
     if (this.model.getHerosLifepoints() == 0) {
       this.end();
       this.view.scrollbox2.childNodes[this.model.monsterIndex].classList.add("selected");
       this.view.lbl9.innerHTML = this.model.emptyCondition;
+      this.view.winnerIcon.src = "./images/bad_omen.png"
+      this.view.winningTeam.innerHTML = "monsters"
     } else if (this.model.getMonstersLifepoints() == 0) {
       this.end();
       this.view.scrollbox1.childNodes[this.model.heroIndex].classList.add("selected");
       this.view.lbl8.innerHTML = this.model.emptyCondition;
+      this.view.winnerIcon.src = "./images/hero_of_the_village.png"
+      this.view.winningTeam.innerHTML = "heros"
     } else {
       this.view.lbl1.innerHTML = this.model.round;
       this.view.scrollbox1.childNodes[this.model.heroIndex].classList.add("selected");
@@ -49,6 +59,7 @@ class Controller {
   end() {
     this.view.btn4.disabled = true;
     this.view.btn4.classList.add("disabled");
+    this.view.winner.classList.remove("hidden");
   }
 
   changeHero(index) {
@@ -65,6 +76,10 @@ class Controller {
     this.model.round++;
     this.model.newMonster()
     this.updateView();
+  }
+
+  reloadPressed() {
+    location.reload();
   }
 }
 
