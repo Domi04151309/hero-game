92f9546c99e5aded05c3c937b8ee046c845694a8
Dominik <36272047+Domi04151309@users.noreply.github.com>
2020-06-22 14:52:56 +0200

    Refix variable declaration

--- js/sketch.js
+++ js/sketch.js
@@ -2,4 +2,4 @@ import Model from './model/Model.js';
 import View from './view/View.js';
 import Controller from './controller/Controller.js';
 
-const app = new Controller(new View(), new Model());
+window.app = new Controller(new View(), new Model());
