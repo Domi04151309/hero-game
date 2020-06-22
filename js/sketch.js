a151c923c962ba0d75632b75e8da63e0a36aab17
Dominik <36272047+Domi04151309@users.noreply.github.com>
2020-06-22 14:30:14 +0200

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -2,4 +2,4 @@ import Model from './model/Model.js';
 import View from './view/view.js';
 import Controller from './controller/Controller.js';
 
-const app = new Controller(new View(), new Model());
+window.app = new Controller(new View(), new Model());
